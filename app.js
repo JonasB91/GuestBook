d8d6a4aecd8ee38dff865296d06a11d9868c5bb7
JonasB91 <jonas.backas@hotmail.com>
2023-11-16 11:54:01 +0100

    spara lite server info

--- app.js
+++ app.js
@@ -2,5 +2,15 @@
 
 let express = require("express");
 let app = express()
-let port = 3000;
+let port = 8080; // kör på port 8080
+
+
+let httpserver = app.listen(port, function() {
+    console.log(`Webbservern körs på ${port}`);
+});
+
+//Routar mig till /views/index.html sidan
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html")
+});
 
